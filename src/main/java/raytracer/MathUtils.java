2c32f3f8dfa83fa7e737a0e5c4a25f64c5b303c6
Juha Komulainen <komu@iki.fi>
2010-04-20 19:10:58 +0300

    Added solver for quadratic equations.

--- src/main/java/raytracer/MathUtils.java
+++ src/main/java/raytracer/MathUtils.java
@@ -2,11 +2,33 @@ package raytracer;
 
 public final class MathUtils {
 
+    private static final float[] EMPTY_FLOAT_ARRAY = new float[0];
+    
     public static float square(float x) {
         return x*x;
     }
     
     public static float sqrt(float x) {
         return (float) Math.sqrt(x);
+    }
+    
+    /**
+     * Returns the roots roots of ax^2+bx+c = 0.
+     */
+    public static float[] rootsOfQuadraticEquation(float a, float b, float c) {
+        float disc = square(b) - 4 * a * c;
+        float divisor = 2*a;
+        
+        if (disc < 0)
+            return EMPTY_FLOAT_ARRAY;
+        
+        if (disc == 0)
+            return new float[] { -b / divisor };
+        
+        float discSqrt = sqrt(disc);
+        return new float[] { 
+            (-b + discSqrt) / divisor,
+            (-b - discSqrt) / divisor
+        };
     }
 }
