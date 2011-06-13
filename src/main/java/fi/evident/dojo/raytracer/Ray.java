31b56b01d726e27fb0c0925989c8da04f395317c
Juha Komulainen <juha.komulainen@evident.fi>
2011-06-13 21:29:33 +0300

    Cosmetic changes.

--- src/main/java/fi/evident/dojo/raytracer/Ray.java
+++ src/main/java/fi/evident/dojo/raytracer/Ray.java
@@ -3,7 +3,7 @@ package fi.evident.dojo.raytracer;
 public final class Ray {
 
     public final Vector3 start;
-    public  final Vector3 direction;
+    public final Vector3 direction;
 
     public Ray(Vector3 start, Vector3 direction) {
         assert start != null;
